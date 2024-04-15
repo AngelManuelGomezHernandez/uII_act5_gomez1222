0173541282a9552fda1e50f4918ec82cf2ebcf13
ANGEL_MANUEL_GOMEZ_HERNANDEZ <a.21308051281222@cbtis128.edu.mx>
2024-04-15 02:05:09 +0000

    actu

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
@@ -24,18 +25,63 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Color(0xff4100ab),
+        title: const Text(
+          "Mostrar imagenes 1222",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+            ),
+            Text(
+              "Imagen en la carpeta",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Color(0xff0f6cb9),
+              ),
+            ),
+            SizedBox(
+              height: 250,
+              width: 350,
+              child: Image.asset(
+                "assets/mono.jpg",
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+            ),
+            Text(
+              "Imagen WEB",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Color(0xff0f6cb9),
+              ),
+            ),
+            SizedBox(
+              height: 250,
+              width: 350,
+              child: Image.network(
+                "https://i.pinimg.com/736x/a9/1b/64/a91b645428fe11bbc21d05dd1de32008.jpg",
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
         ),
       ),
     );
